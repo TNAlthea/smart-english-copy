79fd11d568cd4f136f5dfa9b28612c57f1fccf49
IrfanEsa <72029919+WataKoro@users.noreply.github.com>
2022-05-21 19:40:04 +0700

    Add files via upload

--- database/dbsmartenglish.sql
+++ database/dbsmartenglish.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2022 at 08:51 AM
+-- Generation Time: May 21, 2022 at 02:38 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -24,22 +24,78 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tadmin`
+--
+
+CREATE TABLE `tadmin` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tartikel`
+--
+
+CREATE TABLE `tartikel` (
+  `id` int(10) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `articleImage` longblob NOT NULL,
+  `articleDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tpengajar`
+--
+
+CREATE TABLE `tpengajar` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `education` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `photo` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tprogram`
+--
+
+CREATE TABLE `tprogram` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `schedule` time NOT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tregistrasi`
 --
 
 CREATE TABLE `tregistrasi` (
   `id` int(10) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `grade` varchar(100) NOT NULL,
-  `birthPlace` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `grade` varchar(255) NOT NULL,
+  `birthPlace` varchar(255) NOT NULL,
   `birthDate` date NOT NULL,
-  `parentJob` varchar(100) NOT NULL,
+  `parentJob` varchar(255) NOT NULL,
   `isLocal` tinyint(1) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `phone` int(100) NOT NULL,
-  `programName` varchar(100) NOT NULL,
+  `province` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `programName` varchar(255) NOT NULL,
   `programSchedule` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -48,10 +104,62 @@ CREATE TABLE `tregistrasi` (
 --
 
 --
+-- Indexes for table `tadmin`
+--
+ALTER TABLE `tadmin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `tartikel`
+--
+ALTER TABLE `tartikel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tpengajar`
+--
+ALTER TABLE `tpengajar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tprogram`
+--
+ALTER TABLE `tprogram`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tregistrasi`
 --
 ALTER TABLE `tregistrasi`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tartikel`
+--
+ALTER TABLE `tartikel`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tpengajar`
+--
+ALTER TABLE `tpengajar`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tprogram`
+--
+ALTER TABLE `tprogram`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tregistrasi`
+--
+ALTER TABLE `tregistrasi`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
