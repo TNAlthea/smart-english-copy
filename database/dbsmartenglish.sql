d3493a87edee89ebd29d40f844fc29b8d540756a
IrfanEsa <72029919+WataKoro@users.noreply.github.com>
2022-05-22 13:58:35 +0700

    Add files via upload

--- database/dbsmartenglish.sql
+++ database/dbsmartenglish.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2022 at 02:51 PM
+-- Generation Time: May 22, 2022 at 08:31 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -41,7 +41,7 @@ CREATE TABLE `tadmin` (
 CREATE TABLE `tartikel` (
   `id` int(10) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `text` varchar(8192) NOT NULL,
+  `text` text NOT NULL,
   `articleImage` longblob NOT NULL,
   `articleDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -86,19 +86,30 @@ CREATE TABLE `tregistrasi` (
   `id` int(10) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `grade` varchar(255) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `eduStage` varchar(255) NOT NULL,
+  `eduGrade` varchar(255) NOT NULL,
+  `school` varchar(255) NOT NULL,
   `birthPlace` varchar(255) NOT NULL,
   `birthDate` date NOT NULL,
   `parentJob` varchar(255) NOT NULL,
-  `isLocal` tinyint(1) NOT NULL,
+  `Location` varchar(255) NOT NULL,
   `province` varchar(255) NOT NULL,
   `city` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `programName` varchar(255) NOT NULL,
-  `programSchedule` time NOT NULL
+  `programSchedule` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tregistrasi`
+--
+
+INSERT INTO `tregistrasi` (`id`, `email`, `password`, `fname`, `lname`, `eduStage`, `eduGrade`, `school`, `birthPlace`, `birthDate`, `parentJob`, `Location`, `province`, `city`, `address`, `phone`, `programName`, `programSchedule`) VALUES
+(1, 'irfanesa@upi.edu', '$2y$10$4F0Ywq78AT1RpXEJxsMEiucPZFvRbP1gxTeUjjQ4ZrR2fRnCXsT4W', 'asdasd', 'asdasd', 'junior', '1', 'asdasd', 'asdasd', '2022-05-03', 'PNS', 'Local', 'jakarta', 'java', 'asdasd', '081220830725', 'English Only', '00:11:11'),
+(2, 'irfanmesa13@gmail.com', '$2y$10$AAjOu1agckbcbLmK3Y1MfuGSyrfZK7RJpJ2zAG3ROi3iiZJEs89Be', 'irfan', 'mochamad esa', 'senior', 'Grade 2', 'Alfa Centauri', 'Tulungagung', '2022-04-05', 'other', 'Local', 'java', 'Bandung', 'Margahayu', '081220830725', 'Cram School', 'Friday and Saturday, 19.00-20.30');
 
 --
 -- Indexes for dumped tables
@@ -160,7 +171,7 @@ ALTER TABLE `tprogram`
 -- AUTO_INCREMENT for table `tregistrasi`
 --
 ALTER TABLE `tregistrasi`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
